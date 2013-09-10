e6afa28a092481a4cc93b4623b21257f1c7dbf50
Benjamin Kreuter <brk7bx@virginia.edu>
2013-09-10 10:28:08 -0400

    added more test cases

---
+++ bxoru.c
@@ -0,0 +1,10 @@
+unsigned int alice(unsigned int);
+unsigned int bob(unsigned int);
+void output_alice(unsigned int);
+
+void main(void)
+{
+  unsigned int x = alice(0);
+  unsigned int y = bob(0);
+  output_alice(x ^ y);
+}
---
+++ l-shift.c
@@ -0,0 +1,9 @@
+unsigned int alice(unsigned int);
+unsigned int bob(unsigned int);
+void output_alice(unsigned int);
+
+void main(void)
+{
+  unsigned int x = alice(0);
+  output_alice(x << 1);
+}
