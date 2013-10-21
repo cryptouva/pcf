43fa161020ed57b972aeabd6de226776719b7409
Benjamin Kreuter <brk7bx@virginia.edu>
2013-10-21 11:21:11 -0400

    added lcc pointer analysis

---
+++ lcc-pointer-analysis.lisp
@@ -0,0 +1,86 @@
+;; Basic pointer analysis framework for LCC.  This will make other
+;; dataflow more precise and allow us to handle conditional function
+;; calls correctly, among other things.  Basically, we need to be able
+;; to determine that we have a local pointer, at the very least, and
+;; if that local pointer points to a constant (which is definitely
+;; something we want to know).
+
+(defpackage :lcc-ptranl
+  (:use :cl
+        :utils
+        :lcc-dataflow
+        :lcc-bc
+        :setmap)
+  (:export ptranl-dataflow-funs)
+  (:shadow export import)
+  )
+(in-package :lcc-ptranl)
+
+
+(defun const-gen (constL depL constR depR)
+  (declare (ignore depL depR)
+           (type avl-set constL constR))
+  (the avl-set
+    (set-reduce (lambda (mp x)
+                  (map-insert x constR mp)
+                  )
+                (map-empty)
+                constL)
+    )
+  )
+
+(defun dep-gen (constL depL constR depR)
+  (declare (type avl-set constL depL constR depR))
+  (labels ((add-ptrs (st mp x)
+             (let ((curval (aif (map-find x mp)
+                                (cdr it)
+                                (empty-set)
+                                )
+                     )
+                   )
+               (map-insert x (set-union curval st) mp)
+               )
+             )
+           )
+    (let* ((map1 (set-reduce (papply #'add-ptrs depR)
+                             (map-empty)
+                             constL)
+             )
+           (map2 (set-reduce (papply #'add-ptrs constR)
+                             map1
+                             depL))
+           )
+      (the avl-set (set-reduce (papply #'add-ptrs depR)
+                               map2
+                               depL)
+           )
+      )
+    )
+  )
+
+(defun const-kill (constL depL constR depR)
+  (declare (ignore depL constR depR)
+           (type avl-set constL)
+           (optimize (debug 3) (speed 0)))
+  (the avl-set
+    (set-reduce (lambda (mp x)
+                  (map-insert x (empty-set) mp)
+                  )
+                constL
+                (map-empty)
+                )
+    )
+  )
+
+(defun dep-kill (constL depL constR depR)
+  (declare (ignore constL constR depR)
+           (type avl-set depL))
+  (the avl-set 
+    (set-reduce (lambda (mp x)
+                  (map-insert x (empty-set) mp)
+                  )
+                depL
+                (map-empty)
+                )
+    )
+  )
