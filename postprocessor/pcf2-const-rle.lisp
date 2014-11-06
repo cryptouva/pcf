c040276a25fd84267f65cc394a3dff426672108d
Ben Terner <bt3ze@virginia.edu>
2014-11-05 19:14:30 -0500

    fixing assertion

--- postprocessor/pcf2-const-rle.lisp
+++ postprocessor/pcf2-const-rle.lisp
@@ -331,10 +331,12 @@
     :dep-gen (with-slots (dest op1 op2 truth-table) op
                (with-true-addresses (dest op1 op2)
                  (let ((o1 (rle-map-extract-val op1 flow-data))
-                       (o2 (rle-map-extract-val op2 flow-data)))
+                       (o2 (rle-map-extract-val op2 flow-data))
+                       (op1-val (rle-map-val op1 flow-data))
+                       (op2-val (rle-map-val op2 flow-data)))
                    ;;(break)
-                   (assert (or (equal o1 0)(equal o1 1)(equal o1 'pcf2-block-graph:pcf-not-const)))
-                   (assert (or (equal o2 0)(equal o2 1)(equal o1 'pcf2-block-graph:pcf-not-const)))
+                   (assert (or (equal o1 0)(equal o1 1)(equal op1-val 'pcf2-block-graph:pcf-not-const)))
+                   (assert (or (equal o2 0)(equal o2 1)(equal op2-val 'pcf2-block-graph:pcf-not-const)))
                    (if (or-defined op1 op2 flow-data)
                        (if (and-defined op1 op2 flow-data) ;; if both are constant, we can precompute the gate
                            (progn
@@ -459,9 +461,10 @@
 
 (def-gen-kill mkptr
     :dep-gen (with-slots (dest) op
-               (let ((ptr (rle-map-val dest flow-data)))
-                 (with-true-addresses (dest ptr)
-                   (rle-map-singleton dest ptr))))
+               (with-true-addresses(dest)
+                 (let ((ptr (rle-map-val dest flow-data)))
+                   (with-true-addresses (ptr)
+                     (rle-map-singleton dest ptr)))))
     :const-kill (with-slots (dest) op
                   (with-true-addresses (dest)
                     (rle-singleton dest))))
